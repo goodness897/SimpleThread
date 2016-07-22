457c1027eba9a26e152708a280e22c9522652050
ChunWoo Lee <partway220@naver.com>
2016-07-22 15:28:31 +0900

    Initial commit

---
+++ app/src/main/java/com/example/tacademy/simplethread/PostActivity.java
@@ -0,0 +1,74 @@
+package com.example.tacademy.simplethread;
+
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+
+public class PostActivity extends AppCompatActivity {
+
+    TextView messageView;
+    ProgressBar downloadView;
+
+    Handler mHandler = new Handler(Looper.getMainLooper());
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_post);
+
+        messageView = (TextView)findViewById(R.id.text_message);
+        downloadView = (ProgressBar) findViewById(R.id.progressBar);
+
+        Button btn = (Button)findViewById(R.id.btn_start);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                downloadView.setMax(100);
+                messageView.setText("download start...");
+                new Thread(new Runnable() {
+                    @Override
+                    public void run() {
+                        int progress = 0;
+                        while(progress <= 100) {
+                            mHandler.post(new ProgressRunnable(progress));
+                            try {
+                                Thread.sleep(500);
+                            } catch (InterruptedException e) {
+                                e.printStackTrace();
+                            }
+                            progress += 5;
+
+                        }
+                        mHandler.post(new DoneRunnable());
+                    }
+                }).start();
+            }
+        });
+    }
+
+    class ProgressRunnable implements Runnable {
+        int progress;
+        public ProgressRunnable(int progress) {
+            this.progress = progress;
+        }
+        @Override
+        public void run() {
+            downloadView.setProgress(progress);
+            messageView.setText("progress : " + progress);
+        }
+    }
+
+    class DoneRunnable implements Runnable {
+
+        @Override
+        public void run() {
+            downloadView.setProgress(100);
+            messageView.setText("Complete!");
+        }
+    }
+}
